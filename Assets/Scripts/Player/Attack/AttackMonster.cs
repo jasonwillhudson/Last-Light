4f358ab8b921999a9d2e5b5807ee410de450edcf
jasonwillhudson <38706702+jasonwillhudson@users.noreply.github.com>
2022-11-03 18:11:23 -0700

    fixed trigger

--- Assets/Scripts/Player/Attack/AttackMonster.cs
+++ Assets/Scripts/Player/Attack/AttackMonster.cs
@@ -6,27 +6,27 @@ public class AttackMonster : MonoBehaviour
 {
 
     public GameObject player;
-    private bool attackStatus=false;
-    private bool cooldown=false;
+    private bool attackStatus = false;
+    private bool cooldown = false;
 
     void Update()
     {
         attackStatus = player.GetComponent<Player>().playerControl.getAttackStatus();
         if (!attackStatus) cooldown = false; //reset cooldown
-       
+
     }
-    void OnTriggerEnter2D(Collision2D collision)
+    void OnTriggerEnter2D(Collider2D collision)
     {
 
-        Debug.Log("GameObject1 collided with ");
-        if (collision.collider.gameObject.CompareTag("Enemy") && attackStatus && !cooldown)
+        Debug.Log("GameObject1 collided with " + collision.gameObject.tag);
+        if (collision.gameObject.CompareTag("Enemy") && attackStatus && !cooldown)
         {
-            collision.collider.gameObject.GetComponent<Enemy>().health.getDamaged(20);
+            collision.gameObject.GetComponent<Enemy>().health.getDamaged(20);
             Debug.Log("Attack Monster");
             cooldown = true;
 
         }
 
-        
+
     }
 }
